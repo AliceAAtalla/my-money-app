76dd66182e1959118bed8f360ccf8fefc4690e61
AliceAAtalla <aliceatalla36@gmail.com>
2020-10-16 16:57:01 -0300

    Allow Cors

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,11 +2,13 @@ const PORT = 3003;
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const allowCors = require('./cors');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(allowCors);
 
 app.listen(PORT, () => console.log(`Backend is running on port ${PORT}.`));
 
