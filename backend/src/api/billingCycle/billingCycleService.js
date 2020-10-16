7e0ebeb51cc524cffff9a5a2c2e0b2c4079cd813
AliceAAtalla <aliceatalla36@gmail.com>
2020-10-16 16:13:28 -0300

    Route Count

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -6,19 +6,14 @@ BillingCycle.methods(['get', 'post', 'put', 'delete']);
 // e a validação vai ocorrer tbm no metodo put e não só no post;
 BillingCycle.updateOptions({ new: true, runValidators: true });
 
-/* O metodo find() com parametro {}, faz com que o mongoose busque todos os registros
-na coleção BillingCycles de forma indiscriminada. Caso a consulta não produza nenhum erro,
-na resposta retornamos os documentos. Caso contrário, retornamos um Status Code 500 e a
-mensagem de erro proveniente do banco de dados.
- */
-// BillingCycle.route('get', (_req, res, _next) => {
-//   BillingCycle.find({}, (err, docs) => {
-//     if (!err) {
-//       res.json(docs);
-//     } else {
-//       res.status(500).json({ errors: [error] });
-//     }
-//   });
-// });
+BillingCycle.route('count', (req, res, next) => {
+  BillingCycle.count((err, value) => {
+    if (err) {
+      res.status(500).json({ errors: [err] });
+    } else {
+      res.json({ value });
+    }
+  });
+});
 
 module.exports = BillingCycle;
