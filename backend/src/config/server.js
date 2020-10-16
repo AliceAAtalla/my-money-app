c0b99d22550284b61ffde603ca0b0475c68e5763
AliceAAtalla <aliceatalla36@gmail.com>
2020-10-16 17:00:12 -0300

    Query Parser

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,12 +3,14 @@ const PORT = 3003;
 const express = require('express');
 const bodyParser = require('body-parser');
 const allowCors = require('./cors');
+const queryParser = require('express-query-int');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(allowCors);
+app.use(queryParser());
 
 app.listen(PORT, () => console.log(`Backend is running on port ${PORT}.`));
 
